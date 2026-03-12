806e00f0942386cf284905ad5bc7b5d27ca567c4
Erik Karsten <erik@opcode.xyz>
2026-03-12 16:45:44 +0200

    feat: add overlay, devshell

--- flake.nix
+++ flake.nix
@@ -8,15 +8,14 @@
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   outputs =
-    {
-      self,
-      nixpkgs,
-      wrappers,
-      flake-parts,
-      ...
+    { self
+    , nixpkgs
+    , wrappers
+    , flake-parts
+    , ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ wrappers.flakeModules.wrappers ];
+      imports = [ wrappers.flakeModules.wrappers flake-parts.flakeModules.easyOverlay ];
       systems = nixpkgs.lib.platforms.all;
 
       perSystem =
@@ -27,8 +26,12 @@
             config.allowUnfree = true;
           };
 
-          packages.ccusage = pkgs.callPackage ./packages/ccusage.nix {};
+          packages.ccusage = pkgs.callPackage ./packages/ccusage.nix { };
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ self.packages.${system}.ccusage self.packages.${system}.default ];
+          };
         };
+
       flake.wrappers.default = ./wrapper.nix;
     };
 }
